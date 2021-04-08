17c1f91c1764277220a3ae3a82b1e9ffdc39d3fe
Aris Panagiotides <arispan94@yahoo.gr>
2021-04-08 13:59:34 +0300

    Now passes application's scope as a parameter in getDatabase()

--- app/src/main/java/com/example/projectcompass/CompassApplication.kt
+++ app/src/main/java/com/example/projectcompass/CompassApplication.kt
@@ -1,11 +1,15 @@
 package com.example.projectcompass
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class CompassApplication : Application() {
 
+    private val applicationScope = CoroutineScope(SupervisorJob())
+
     // Because these objects should only be created when they're first needed,
     // rather than at app startup, we're using Kotlin's property delegation: by lazy.
-    private val database by lazy { MeasurementRoomDatabase.getDatabase(this) }
+    private val database by lazy { MeasurementRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { MeasurementRepository(database.measurementDAO()) }
 }
