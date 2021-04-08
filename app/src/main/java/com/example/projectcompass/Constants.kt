e3f8225ff37ef5b0f9bb53e6a0d6872ea17af9e8
Aris Panagiotides <arispan94@yahoo.gr>
2021-04-08 22:31:33 +0300

    Added Location-Tracking constants.

--- app/src/main/java/com/example/projectcompass/Constants.kt
+++ app/src/main/java/com/example/projectcompass/Constants.kt
@@ -14,3 +14,5 @@ const val PASSWORD = "guest"
 /* Location Tracking constants */
 const val LOCATION_PERMISSION_REQUEST_CODE = 1
 const val REQUEST_CHECK_SETTINGS = 2
+const val REQUESTING_LOCATION_UPDATES_KEY = "Requesting location updates"
+const val TRACKING_KEY = "Tracking"
