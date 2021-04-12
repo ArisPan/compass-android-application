5f66122abb2508e3bfc99b92a33361fede75863d
Aris Panagiotides <arispan94@yahoo.gr>
2021-04-12 17:51:13 +0300

    Added constant to serve as a KEY for VALUE allMeasurementsIndexOnClear. This will be used for saving MainActivity's instance's state.

--- app/src/main/java/com/example/projectcompass/Constants.kt
+++ app/src/main/java/com/example/projectcompass/Constants.kt
@@ -16,3 +16,4 @@ const val LOCATION_PERMISSION_REQUEST_CODE = 1
 const val REQUEST_CHECK_SETTINGS = 2
 const val REQUESTING_LOCATION_UPDATES_KEY = "Requesting location updates"
 const val TRACKING_KEY = "Tracking"
+const val INDEX_ON_CLEAR = "All measurements index on clear"
