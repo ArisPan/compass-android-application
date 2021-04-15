94997ff64debcb044ec81165db1272af7218eb6d
Aris Panagiotides <arispan94@yahoo.gr>
2021-04-15 11:51:23 +0300

    Added location request interval constants.

--- app/src/main/java/com/example/projectcompass/Constants.kt
+++ app/src/main/java/com/example/projectcompass/Constants.kt
@@ -15,5 +15,9 @@ const val PASSWORD = "guest"
 const val LOCATION_PERMISSION_REQUEST_CODE = 1
 const val REQUEST_CHECK_SETTINGS = 2
 const val REQUESTING_LOCATION_UPDATES_KEY = "Requesting location updates"
+const val LOCATION_REQUEST_INTERVAL = 12000
+const val LOCATION_REQUEST_FASTEST_INTERVAL = 7000
+
+/* MainActivity Constants */
 const val TRACKING_KEY = "Tracking"
 const val INDEX_ON_CLEAR = "All measurements index on clear"
